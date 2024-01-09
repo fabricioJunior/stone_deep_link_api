5b68ec1acba623a04f294bd5bce18143e2304bfa
fabricioJunior <fabriciojamescarneiro@gmail.com>
2024-01-09 18:17:04 -0300

    start commit

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/fabriciojunior/fvm/versions/3.16.0"
+export "FLUTTER_APPLICATION_PATH=/Users/fabriciojunior/Documents/projetos/stone_deep_link_api/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
