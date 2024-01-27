5e55bbde439a5bcef83f74b8baf962ad16e14cb2
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2024-01-27 16:48:41 -0300

    delete all files

--- lib/stone_deep_link_api.dart
+++
@@ -1,8 +0,0 @@
-
-import 'stone_deep_link_api_platform_interface.dart';
-
-class StoneDeepLinkApi {
-  Future<String?> getPlatformVersion() {
-    return StoneDeepLinkApiPlatform.instance.getPlatformVersion();
-  }
-}
