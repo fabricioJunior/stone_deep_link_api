2aaa4daafeea1ea46b17afa59662e58a3b08cbc0
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2024-02-14 16:07:12 -0300

    resolvendo dependencias

---
+++ lib/injections.dart
@@ -0,0 +1,9 @@
+import 'package:smart_pag_contract/injections/injections.dart';
+import 'package:stone_deep_link/stone_deep_link.dart';
+
+import 'pagamento.dart';
+
+void resolveDependenciasStoneDeepLink() {
+  sl.registerFactory<PagamentoContract>(() => Pagamento());
+  pagamentoIsImplements = true;
+}
